f7d81ce0ef6e90379202b445e8c4c1c6be4efcba
Olumide2596 <olumide2596@gmail.com>
2022-08-21 21:38:26 +0100

    to be completed

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+// const inputFileds = document.querySelectorAll('input');
+
+// document.querySelector('.submit').addEventListener('click', function (e) {
+// 	console.log('clicked');
+// 	console.log(inputFileds.forEach(input => input.getAttribute('type')));
+// 	// input.style.border = '1px solid red';
+// });
