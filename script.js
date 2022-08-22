2437ad5a8d5bac5f124174c3e0cd2e9bf4f0c25f
Olumide2596 <olumide2596@gmail.com>
2022-08-22 15:44:18 +0100

    commit

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 'use strict';
 const inputFileds = document.querySelectorAll('input');
 const box = document.querySelectorAll('.box');
-const errorImg = document.querySelectorAll('.errorImg');
+// const errorImg = document.querySelectorAll('.errorImg');
 
 document.querySelector('.submit').addEventListener('click', function (e) {
 	console.log('clicked');
 	inputFileds.forEach(input => {
 		if (input.value.trim() === '') {
 			input.style.border = '1px solid red';
-			input.value = console.log('EMPTY');
 			input.insertAdjacentHTML(
 				'afterend',
 				`<p style='color:red;text-align:right;'>${input.name} cannot be empty</p>`
@@ -16,3 +15,5 @@ document.querySelector('.submit').addEventListener('click', function (e) {
 		}
 	});
 });
+
+console.log(box);
